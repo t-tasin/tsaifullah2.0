7c824cacde23648f4c1d69ed879050f584040a20
t-tasin <tasin0.net@gmail.com>
2024-10-13 17:51:32 -0400

    Add: About page

---
+++ src/components/Technology.jsx
@@ -0,0 +1,16 @@
+import { RiReactjsLine } from "react-icons/ri";
+
+const Technology = () => {
+  return (
+    <div className="border-b border-neutra-800 pb-24">
+      <h1 className="my-20 text-center text-4xl">Technology</h1>
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        <div className="roudned-2xl border-4 border-neutral-800 p-4">
+          <RiReactjsLine className=" text-7xl text-cyan-400" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Technology;
