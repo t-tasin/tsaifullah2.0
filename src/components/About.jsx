3d61517fccb17d43669b17b0af68e68af6f336a0
t-tasin <tasin0.net@gmail.com>
2024-10-13 21:25:06 -0400

    Hero and About animated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import { ABOUT_TEXT } from "../constants";
 import aboutImg from "/Users/tasin/Desktop/Codebase/tsaifullah2.0/src/assets/about.jpg";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
@@ -9,16 +10,26 @@ const About = () => {
         <span className="text-neutral-500"> ME</span>
       </h1>
       <div className="flex flex-wrap">
-        <div className="w-full lg:w-1/2 lg:p-8">
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:p-8"
+        >
           <div className="flex items-center justify-center">
             <img className="rounded-2xl max-w-md" src={aboutImg} alt="about" />
           </div>
-        </div>
-        <div className="w-full lg:w-1/2">
+        </motion.div>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2"
+        >
           <div className="flex justify-center lg:justify-start">
             <p className="my-2 maxw-xl py-6">{ABOUT_TEXT}</p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
